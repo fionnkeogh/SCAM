c6e3b4331fe7f0458830fe56018a6ae048086a61
Carsten Scholl <carsten.scholl@uni-jena.de>
2023-06-04 23:32:49 +0200

    expanded payoff constructor

--- macrophage_vs_conidia/payoff.py
+++ macrophage_vs_conidia/payoff.py
@@ -2,7 +2,27 @@ class Payoff():
 
 
 
-    def __init__(self, mpp = 1, mpa = 0, map = 2, maa = -2, cpp = 1, cpa = 0, cap = 2, caa = -2):
+    def __init__(self, *args):
+
+        if len(args) == 0:
+            mpp, mpa, map, maa, cpp, cpa, cap, caa = 1, 0, 2, -2, 1, 0, 2, -2
+
+        elif isinstance(args[0], int):
+            mpp, mpa, map, maa, cpp, cpa, cap, caa = args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7]
+        
+        elif isinstance(args[0], str):
+            if args[0] == 'patho1':
+                mpp, mpa, map, maa, cpp, cpa, cap, caa = 5, 0, 3, 1, 5, 0, 6, 1
+            
+            elif args[0] == 'patho2':
+                mpp, mpa, map, maa, cpp, cpa, cap, caa = 5, 0, 3, 1, 5, 0, 4, 1
+
+            elif args[0] == 'alien':
+                mpp, mpa, map, maa, cpp, cpa, cap, caa = 3, 1, 0, 2, 2, 0, 1, 1
+
+        
+
+
         self.mpp = mpp
         self.mpa = mpa
         self.map = map
