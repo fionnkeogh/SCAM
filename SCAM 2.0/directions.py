77e0ce91071b7e20372365d160f37879762c2867
Fionn Keogh <fionndaire.keogh@outlook.de>
2024-08-03 14:33:45 +0200

    Reset

--- SCAM 2.0/directions.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-class directions(Enum):
-    """Enumerator class for the 4 possible directions an agent can face"""
-    NORTH = 0
-    EAST = 1
-    SOUTH = 2
-    WEST = 3
